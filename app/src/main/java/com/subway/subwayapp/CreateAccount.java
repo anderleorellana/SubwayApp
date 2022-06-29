179f91c177b4df2c849fb4b217bd2df48496c400
ThonyZV <anthonyzv08@gmail.com>
2022-06-29 00:22:37 -0500

    create account

---
+++ app/src/main/java/com/subway/subwayapp/CreateAccount.java
@@ -0,0 +1,19 @@
+package com.subway.subwayapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+
+
+
+    }
+}
--- app/src/main/java/com/subway/subwayapp/MainActivity.java
+++ app/src/main/java/com/subway/subwayapp/MainActivity.java
@@ -1,15 +1,30 @@
 package com.subway.subwayapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     int numero;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+
+    }
+
+    public void onClick(View v) {
+        // Do something in response to button click
+        Intent i = new Intent(this, CreateAccount.class);
+        System.out.println( "gaaa");
+        startActivity(i);
     }
 }
